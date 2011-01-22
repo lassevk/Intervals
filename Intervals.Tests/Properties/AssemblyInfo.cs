cc74eb27717d5e90425137113601d42f8498c870
Lasse V. Karlsen <lasse@vkarlsen.no>
2011-01-22 20:30:05 +0100

    Initial commit.

---
+++ Intervals.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Intervals")]
+[assembly: AssemblyDescription("Unit-tests for Intervals")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Intervals")]
+[assembly: AssemblyCopyright("Copyright (C) Lasse V. Karlsen 2010-2011, All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("3dcb94e2-e97c-4324-a9a0-7f6f7b21a870")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
---
+++ Intervals/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Intervals")]
+[assembly: AssemblyDescription("Interval Calculation Class Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Intervals")]
+[assembly: AssemblyCopyright("Copyright (C) Lasse V. Karlsen 2010-2011, All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("7ebb24bb-6be1-4eb2-87a1-cb34063710f6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+#if USE_RELEASE_KEY
+[assembly:
+    InternalsVisibleTo(
+        "Intervals.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010059921c7f53775f0382b28e3fc23ae1071edf054c681b933fc225417ca6bd3abb23b815c428e5b0ac871b5d2423c7f4424171dae25f12918a256912be7cb1244ebb0528df91a5f89216b0f9092368ca234c3c2208114b5bf4e1b7cd8e9e4ebc930bf54db265a7437162f7379a64b8013770ede01e49f76aaee95ebaa3d8f7bccd"
+        )]
+#else
+
+[assembly:
+    InternalsVisibleTo(
+        "Intervals.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100bdb1d6437a0dbc6d7493f10e26da95d5b084c701290debcce65d0e85651646aa12bd4a3a16e35be448d66679de645bdd20f0e15f968873c96df9f63765daad26970f2ecfef34778e1915f3e186550a77b78e056bc198a32335caad6870b7481781ed9dd7dbd5d4b79c5c22c28bc95324cef0acebb06e0bb85c252f7dc48f7ece"
+        )]
+#endif
